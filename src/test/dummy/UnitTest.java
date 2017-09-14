a562b00efb25f06a1036723dd26e86f37d94b359
gogsadmin <RedHat$1>
2017-09-14 07:04:20 +0100

    reverted to integration tests

--- src/test/dummy/UnitTest.java
+++ src/test/dummy/UnitTest.java
@@ -1,16 +1,21 @@
 package net.atos.openbanking.api;
 
 import org.apache.http.HttpStatus;
-import org.jboss.resteasy.mock.MockHttpRequest;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
 
-import javax.json.JsonObject;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
+
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(JUnit4.class)
 public class UnitTest {
@@ -20,7 +25,11 @@ public class UnitTest {
   @Test
   public void should_return_bad_request() throws Exception {
     //Given
-    HttpHeaders headers = MockHttpRequest.create("GET","nox://foo").header("foo","bar").getHttpHeaders();
+    HttpHeaders headers = mock(HttpHeaders.class);
+    when(headers.getRequestHeader(anyString()))
+            .thenReturn(Arrays.asList(""));
+    when(headers.getRequestHeader("foo"))
+            .thenReturn(Arrays.asList("bar"));
     //When
     Response response = test.getAccounts(headers);
     //Then
@@ -30,10 +39,13 @@ public class UnitTest {
   @Test
   public void should_return_ok() throws Exception {
     //Given
-    HttpHeaders headers = MockHttpRequest.create("GET","nox://foo")
-                            .header("x-fapi-financial-id","this-is-a-test-id")
-                            .header("Authorization","Bearer: cMotDibBl3r")
-                            .getHttpHeaders();
+    HttpHeaders headers = mock(HttpHeaders.class);
+    when(headers.getRequestHeader(anyString()))
+            .thenReturn(Arrays.asList(""));
+    when(headers.getRequestHeader("x-fapi-financial-id"))
+            .thenReturn(Arrays.asList("this-is-a-test"));
+    when(headers.getRequestHeader("Authorization"))
+            .thenReturn(Arrays.asList("Bearer: cMotDibBl3r"));
     //When
     Response response = test.getAccounts(headers);
     //Then
